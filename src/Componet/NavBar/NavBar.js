763ec531cd9bbb3979c083631bd9a8438e4182bb
farhadctg22201~ <farhadctg222@gmail.com>
2023-10-17 11:34:34 +0600

    d

--- src/Componet/NavBar/NavBar.js
+++ src/Componet/NavBar/NavBar.js
@@ -7,7 +7,7 @@ const NavBar = () => {
   return (
     <div className='head'>
       <div className="heading col-md-6 col-sm-4 col-lg-6">
-        <h1>Mohammad Farhad Uddin</h1>
+        <h1 className='title'>Mohammad Farhad Uddin</h1>
         <h3>Hi, I am <b>farhad</b> and I work as a web developer. I am a software engineer who loves to create websites as well as apps for people. I think that people should look at the bigger picture when they are building something. I love to work in groups where everyone can voice their opinions and ideas.</h3>
         <button>Hire Me</button>
         </div>
