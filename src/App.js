97d2fe5de59d126f7c151e12d0f43b1b3a3dede1
farhadctg22201~ <farhadctg222@gmail.com>
2023-10-17 14:32:02 +0600

    d

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import {BrowserRouter,Route,Routes}from 'react-router-dom'
 import Home from './Componet/Home/Home';
 import NavBar from './Componet/NavBar/NavBar';
+import Profil from './Componet/Profile/Profil';
 
 function App() {
   return (
   <BrowserRouter>
  <NavBar></NavBar>
+ <Profil></Profil>
   <Routes>
    <Route></Route>
   </Routes>
--- src/Componet/NavBar/NavBar.js
+++ src/Componet/NavBar/NavBar.js
@@ -6,15 +6,15 @@ const NavBar = () => {
 
   return (
     <div className='head'>
-      <div className="heading col-md-6 col-sm-4 col-lg-6">
-        <h1 className='title'>Mohammad Farhad Uddin</h1>
-        <h3>Hi, I am <b>farhad</b> and I work as a web developer. I am a software engineer who loves to create websites as well as apps for people. I think that people should look at the bigger picture when they are building something. I love to work in groups where everyone can voice their opinions and ideas.</h3>
+      <div className="heading">
+        <h1 className='title'>I'm Farhad Uddin</h1>
+        <p>Hi, I am <b>farhad</b> and I work as a web developer. I am a software engineer who loves to create websites as well as apps for people. I think that people should look at the bigger picture when they are building something. I love to work in groups where everyone can voice their opinions and ideas.</p>
         <button>Hire Me</button>
         </div>
 
    
   
-      <div className="img-head col-md-6 col-sm-4 col-lg-6">
+      <div className="img-head">
         <img src={image} alt="" />
       </div>
 
