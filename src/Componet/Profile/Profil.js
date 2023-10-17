5fb9dae4ad442aa0dac8373384168beb42964135
farhadctg22201~ <farhadctg222@gmail.com>
2023-10-17 15:52:53 +0600

    d

--- src/Componet/Profile/Profil.js
+++ src/Componet/Profile/Profil.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Profil.css'
 
 const Profil = () => {
     return (
-        <div>
+        <div className='Profile'>
             <h1>Profile Update</h1>
         </div>
     );
