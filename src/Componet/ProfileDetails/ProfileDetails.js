2ebc1ec619a5d8635b70337c60790901b1b69cd0
farhadctg22201~ <farhadctg222@gmail.com>
2023-10-19 14:54:55 +0600

    d

--- src/Componet/ProfileDetails/ProfileDetails.js
+++ src/Componet/ProfileDetails/ProfileDetails.js
@@ -2,17 +2,42 @@ import React from 'react';
 import './ProfileDetails.css'
 
 const ProfileDetails = () => {
+ const justify = {
+    textAlign:'justify',
+    margin:'10px '
+ }
    
-    const aboute = {
-        name:'Aboute Me',
-        detail: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui autem necessitatibus sint vel alias voluptas sunt natus nobis, modi quo, reprehenderit culpa voluptatem quae dolores inventore tempora, doloribus minus nostrum? Quidem incidunt dolores quisquam iure. Cumque earum laudantium illo culpa magnam animi sequi dolor laboriosam tenetur eveniet minus quibusdam officia vero, rem accusamus excepturi perspiciatis, a blanditiis quod debitis necessitatibus ipsum praesentium porro doloribus! Error, molestias. Corrupti soluta sequi non unde libero, obcaecati quae ut nulla ipsum quo autem sit, tempore eveniet, repudiandae nostrum porro! Sint voluptatum tenetur a eius eveniet eos iste praesentium, suscipit aliquam aliquid magni illo voluptate?'
-    }
     
     return (
         <div className='ProfileDetails'>
             <div className="propsTitle">
-                <h1 id='propsTitle'>{aboute.name}</h1>
-                <p>{aboute.detail}</p>
+                <h1 id='propsTitle'>Whate is MERN Stack?</h1>
+                <p style={justify}>MERN Stack is a collection of powerful technologies and robust, used to develop scalable master web applications comprising backend, front-end, and database components. It is JavaScript that is used for the faster and easier development of full-stack web applications. MERN Stack is a technology that is a user-friendly full-stack JavaScript framework for building applications and dynamic websites.<br></br>MERN Stack consists of four main components or can say four main technologies:</p>
+               
+                <img src="" alt="" />
+                <ul>
+                <ol>
+               
+                <li> <b>M</b> stands for MongoDB ( Database ), mainly used for preparing document database and is a NoSQL (Non-Structured Query Language ) Database System</li>
+               
+                    <li>
+
+                    <b> E </b> stands for Express, mainly used for developing Node.js web framework
+                    </li>
+                
+              
+                    <li>
+                    <b>R</b> stands for React, mainly used for developing a client-side JavaScript framework
+                    </li>
+               
+              
+                    <li>
+                    <b>N</b> stands for js, mainly used for developing the premier JavaScript web server
+                    </li>
+                </ol>
+                </ul>
+                <p style={justify}>Each of these four technologies plays an important role in providing an end-to-end framework for the developers. Even these four technologies play an important role in the development process of web applications.<br></br><br></br><br></br>Before MERN stack, it was earlier named as MEAN stack, MERN Stack is one of the variations of MEAN, here MEAN is also comprises of four components or say four different technologies, i.e., M is for MongoDB, ' E ' is for Express, ' A ' is for Angular.js, and ' N ' is for Node, here if you will observe, then you can identify that in MEAN, ' A ', i.e., Angular.js is replaced by ' R ', i.e., React.js in MERN, the main reason behind is - MERN Stack is mainly used for faster development of smaller applications as compared with MEAN, MEAN stack is a mainly better option for large-scale applications. Still, it takes more time for the development of smaller applications. They also both have different structures comparatively. </p>
+               
             </div>
           
             
