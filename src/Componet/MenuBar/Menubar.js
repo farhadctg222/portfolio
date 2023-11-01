53cf695895425119d12e54516b53b3a59870c256
Mohammad Farhad <134843834+farhadctg222@users.noreply.github.com>
2023-11-02 01:34:21 +0600

    Update Menubar.js
    ds

--- src/Componet/MenuBar/Menubar.js
+++ src/Componet/MenuBar/Menubar.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './Menubar.css'
+import { Link } from 'react-router-dom';
 
 const Menubar = () => {
+    const navber = ()=>{
+       const ddd = document.getElementById('ddddd')
+       ddd.innerHTML = `document.getElementById('menu-bar')`
+    }
+
+   
     return (
         <div>
-            <div class="menu-toggle" id="menu-toggle">&#9776;</div>
+          <div class="menu-toggle" id="menu-toggle">
+            <div className="menubarsIcon" id='ddddd' onClick={navber}>
+            <Link class="menuBar">&#9776;</Link></div>
+            <div className="mdfarhad"><h3>mdfarhad</h3>
+          
+         
+          
+          </div>
+          
+          </div>
         <div class="menu-bar" id="menu-bar">
             <ul>
                 <li><a href="#">Home</a></li>
