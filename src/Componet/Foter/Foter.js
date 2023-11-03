d67bcbed18e35378bafa9167c0fd726c553b4d45
Mohammad Farhad <134843834+farhadctg222@users.noreply.github.com>
2023-11-03 20:53:54 +0600

    Update Foter.js
    d

--- src/Componet/Foter/Foter.js
+++ src/Componet/Foter/Foter.js
@@ -2,9 +2,18 @@ import React from 'react';
 import './Foter.css'
 const Foter = () => {
     return (
+        <>
         <div>
            <h1 className='profiles'>Update Coming Sooon.....................</h1>
+
         </div>
+        <footer>
+        <div class="footer-content">
+            <p>&copy; 2023 Mohammad Farhad Uddin</p>
+            <p>Powered by Farhad </p>
+        </div>
+    </footer>
+        </>
     );
 };
 
